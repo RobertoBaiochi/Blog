9012702f7442d25534670bf9ab8deea20438efa6
RobertoBaiochi <robertombbaiochi@gmail.com>
2023-05-06 01:04:57 -0300

    feat: mock HtmlComponent

--- src/components/HtmlComponent/stories.tsx
+++ src/components/HtmlComponent/stories.tsx
@@ -1,20 +1,19 @@
 import { Meta, StoryFn } from '@storybook/react';
 import HtmlComponent, { HtmlComponentProps } from '.';
 
+import mock from './mock';
+
 export default {
   title: 'HtmlComponent',
   component: HtmlComponent,
   args: {
-    html: `Lorem ipsum dolor sit amet consectetur adipisicing elit.
-    Incidunt saepe debitis distinctio facilis quam voluptatem quae eos corrupti
-    aut praesentium eius ipsa voluptates similique possimus voluptas nam dolore,
-    sed maxime.`,
+    html: mock,
   },
 } as Meta;
 
 export const Template: StoryFn<HtmlComponentProps> = (args) => {
   return (
-    <div style={{ maxWidth: '64rem' }}>
+    <div style={{ maxWidth: '80rem', margin: '0 auto' }}>
       <HtmlComponent {...args} />
     </div>
   );
